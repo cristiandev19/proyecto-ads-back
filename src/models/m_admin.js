110e233a6b08c42b6b73b2ac82f59f83fb74ecb2
cristiandev <cristiansotomayor.dev@gmail.com>
2021-01-28 23:24:24 -0500

    modelo insert rol

--- src/models/m_admin.js
+++ src/models/m_admin.js
@@ -1,7 +1,7 @@
 const { pgInstance } = require('../databases/postgres');
 
 exports.insertRol = (desc_rol) => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     pgInstance.one(
       'INSERT INTO public.rol(desc_rol) VALUES ($1) RETURNING id_rol, desc_rol;',
       [desc_rol]
@@ -10,24 +10,76 @@ exports.insertRol = (desc_rol) => {
         return resolve(data);
       })
       .catch(error => {
-        return resolve(error);
+        return reject(error);
       })
   });
 }
 
 
-exports.getRoles = (desc_rol) => {
-  return new Promise((resolve) => {
+exports.getRoles = () => {
+  return new Promise((resolve, reject) => {
     pgInstance.any(
       'SELECT * FROM rol;',
-      [desc_rol]
+      []
     )
       .then(data => {
         console.log('data', data)
         return resolve(data);
       })
       .catch(error => {
-        return resolve(error);
+        return reject(error);
+      })
+  });
+}
+
+
+exports.insertProducto = (precio, stock, desc_producto) => {
+  return new Promise((resolve, reject) => {
+    pgInstance.one(
+      `INSERT INTO public.producto(precio, stock, desc_producto)
+            VALUES ($1, $2, $3)
+         RETURNING id_producto, precio, stock, desc_producto;`,
+      [precio, stock, desc_producto]
+    )
+      .then(data => {
+        return resolve(data);
+      })
+      .catch(error => {
+        return reject(error);
+      })
+  });
+}
+
+
+
+exports.getProductos = () => {
+  return new Promise((resolve, reject) => {
+    pgInstance.any(
+      'SELECT * FROM producto;',
+      []
+    )
+      .then(data => {
+        console.log('data', data)
+        return resolve(data);
+      })
+      .catch(error => {
+        return reject(error);
+      })
+  });
+}
+
+exports.getBoletas = () => {
+  return new Promise((resolve, reject) => {
+    pgInstance.any(
+      'SELECT * FROM boleta;',
+      []
+    )
+      .then(data => {
+        console.log('data', data)
+        return resolve(data);
+      })
+      .catch(error => {
+        return reject(error);
       })
   });
 }
