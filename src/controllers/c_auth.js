6089cd5e881380619143eb76006cf337282e62d9
cristiandev <cristiansotomayor.dev@gmail.com>
2021-01-28 23:24:13 -0500

    cambios en auth

---
+++ src/controllers/c_auth.js
@@ -0,0 +1,29 @@
+const m_auth = require('../models/m_auth');
+const { MESSAGE_API } = require('../config/constants');
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const result = await m_auth.login(email, password);
+    return res.status(200).send({
+      message: MESSAGE_API.SELECT_SUCCESS
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+exports.registerUser = async (req, res, next) => {
+  try {
+    const { nombres, email, password } = req.body;
+    const result = await m_auth.registerUser(nombres, email, password);
+    return res.status(200).send({
+      message: MESSAGE_API.SELECT_SUCCESS,
+      user: {
+        ...result
+      }
+    });
+  } catch (error) {
+    next(error);
+  }
+}
