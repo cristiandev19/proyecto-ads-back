7283b368c55764adce640583e217cce5ea45e59f
cristiandev <cristiansotomayor.dev@gmail.com>
2021-01-30 17:02:26 -0500

    cambiando constantes

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,6 +2,7 @@
 exports.MESSAGE_API = {
   INSERT_SUCCESS: 'Insertado correctamente',
   UPDATE_SUCCESS: 'Actualizado correctamente',
-  SELECT_SUCCESS: 'Datos devueltos correctamente'
+  SELECT_SUCCESS: 'Datos devueltos correctamente',
+  REMOVE_SUCCESS: 'Datos removidos correctamente'
 }
 
